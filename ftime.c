bd4da47e3f08ea306206eeed165fab6c84d331c6
Joel Edwards <joeledwards@gmail.com>
2017-08-05 00:16:53 -0600

    Added the iso and bash formatting commands.

--- ftime.c
+++ ftime.c
@@ -12,6 +12,16 @@
 #define MINUTE 60000000000LL
 #define HOUR 3600000000000LL
 
+
+#define COLOR_OFF     "\x1B[0m"
+#define COLOR_RED     "\x1B[31m"
+#define COLOR_GREEN   "\x1B[32m"
+#define COLOR_YELLOW  "\x1B[33m"
+#define COLOR_BLUE    "\x1B[34m"
+#define COLOR_MAGENTA "\x1B[35m"
+#define COLOR_CYAN    "\x1B[36m"
+#define COLOR_WHITE   "\x1B[37m"
+
 uint64_t getNanoTimestamp(void) {
     long nanos; // Milliseconds
     time_t timestamp;  // Seconds
@@ -28,6 +38,37 @@ uint64_t getNanoTimestamp(void) {
     nanoTime += nanos;
 
     return nanoTime;
+}
+
+struct tm_ext {
+  struct tm *dateTime;
+  uint16_t millis;
+};
+
+struct tm_ext *getUtcDateTime(void) {
+  struct tm_ext timeInfo;
+  uint64_t nanoTs;
+  uint32_t millis;
+  time_t timestamp;
+  struct tm *dateTime;
+
+  nanoTs = getNanoTimestamp();
+  timestamp = nanoTs / 1000000000;
+  dateTime = gmtime(&timestamp);
+
+  timeInfo = (struct tm_ext) { .dateTime = dateTime, .millis = nanoTs / 1000000 & 1000 };
+  
+  return &timeInfo;
+}
+
+struct tm *getDateTime(void) {
+  time_t timestamp;
+  struct tm *dateTime;
+  
+  timestamp = time(NULL);
+  dateTime = localtime(&timestamp);
+  
+  return dateTime;
 }
 
 void printNanoTime(uint64_t nanoTime) {
@@ -60,6 +101,8 @@ void printNanoTime(uint64_t nanoTime) {
 void usage(void) {
   printf("Usage: time format <nano_timestamp>\n");
   printf("       time <ns|us|ms|s|m|h>\n");
+  printf("       time iso\n");
+  printf("       time bash\n");
 }
 
 int main(int argc, char **argv) {
@@ -77,6 +120,31 @@ int main(int argc, char **argv) {
     printf("%lld\n", getNanoTimestamp() / MINUTE);
   } else if (argc == 2 && strcmp(argv[1], "h") == 0) {
     printf("%lld\n", getNanoTimestamp() / HOUR);
+  } else if (argc == 2 && strcmp(argv[1], "iso") == 0) {
+    struct tm_ext *timeInfo = getUtcDateTime();
+    struct tm *dt = timeInfo->dateTime; 
+    printf("%04d-%02d-%02dT%02d:%02d:%02d.%03dZ\n",
+        dt->tm_year + 1900,
+        dt->tm_mon + 1,
+        dt->tm_mday,
+        dt->tm_hour,
+        dt->tm_min,
+        dt->tm_sec,
+        timeInfo->millis
+    );
+  } else if (argc == 2 && strcmp(argv[1], "bash") == 0) {
+    struct tm *dt = getDateTime();
+    char *dateColor = COLOR_GREEN;
+    char *timeColor = COLOR_YELLOW;
+    char *noColor = COLOR_OFF;
+    printf("%s%04d%s-%s%02d%s-%s%02d%s %s%02d%s:%s%02d%s:%s%02d%s\n",
+        dateColor, dt->tm_year + 1900, noColor,
+        dateColor, dt->tm_mon + 1, noColor,
+        dateColor, dt->tm_mday, noColor,
+        timeColor, dt->tm_hour, noColor,
+        timeColor, dt->tm_min, noColor,
+        timeColor, dt->tm_sec, noColor
+    );
   } else if (argc == 3 && strcmp(argv[1], "format") == 0) {
     nanoTime = atoll(argv[2]);
     printNanoTime(nanoTime);
